1abd970f6b60b79fd54f8fb3ae92ecf35cb2d7f9
sashasimulik <simik9697@gmail.com>
2020-05-26 21:53:06 +0300

    Добавьте файлы проекта.

---
+++ OOP_lab_3_18_1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace OOP_lab_3_18_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(new char[] { '\n', '\r', ' ', ':', ';', '.', ',', '?', '!', '(', ')', '{', '}', '[', ']', '@', '#', '№', '$', '^', '%', '&', '*', '/', '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string longestWord = words[0];
+
+            foreach (string word in words)
+            {
+                if (word.Length > longestWord.Length)
+                {
+                    longestWord = word;
+                }
+            }
+
+            string str = "";
+
+            foreach (string word in words)
+            {
+                int k = 0;
+
+                foreach (char leter in word)
+                {
+                    for (char c = 'a'; c <= 'z'; ++c)
+                    {
+                        if (leter == c)
+                        {
+                            ++k;
+                        }
+                    }
+
+                    for (char c = 'A'; c <= 'Z'; ++c)
+                    {
+                        if (leter == c)
+                        {
+                            ++k;
+                        }
+                    }
+                }
+
+                if (k != word.Length)
+                {
+                    str += word + " ";
+                }
+            }
+
+            Console.WriteLine("\n{0}", str);
+            Console.WriteLine("\nНайдовше слово: {0}", longestWord);
+        }
+    }
+}
